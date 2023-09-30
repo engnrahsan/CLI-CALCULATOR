a0b76b1f019e270d3733a6108247c3b551c40c6a
Ahsan ali <engnrahsanali@gmail.com>
2023-09-30 15:22:03 +0500

    first commit

---
+++ node_modules/engr-calculator/index.js
@@ -0,0 +1,48 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+var operators;
+(function (operators) {
+    operators["Add"] = "Addition";
+    operators["Subtraction"] = "Subtraction";
+    operators["Multiplication"] = "Multiplication";
+    operators["Division"] = "Division";
+})(operators || (operators = {}));
+const prompt = inquirer.createPromptModule();
+let answer = await inquirer.prompt([
+    {
+        type: "input",
+        name: "num1",
+        message: "Enter first number"
+    },
+    {
+        type: "input",
+        name: "num2",
+        message: "Enter second number"
+    },
+    {
+        type: "list",
+        name: "operator",
+        message: "Select operator",
+        choices: Object.values(operators)
+    },
+]);
+// const selectedoperator= answer.operators as operators;
+const number1 = parseFloat(answer.num1);
+const number2 = parseFloat(answer.num2);
+if (answer.operator == "Addition") {
+    let rsult = number1 + number2;
+    console.log(chalk.yellowBright `Result is:${rsult}`);
+}
+else if (answer.operator == "Subtraction") {
+    let rsult = number1 - number2;
+    console.log(chalk.bgRedBright `Result is:${rsult}`);
+}
+else if (answer.operator == "Multiplication") {
+    let rsult = number1 * number2;
+    console.log(chalk.greenBright `Result is:${rsult}`);
+}
+else if (answer.operator == "Division") {
+    let rsult = number1 / number2;
+    console.log(chalk.bgBlueBright `Result is:${rsult}`);
+}
